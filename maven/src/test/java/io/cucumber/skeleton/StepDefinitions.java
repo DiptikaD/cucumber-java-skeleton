3027684fdc68f09b664481e72bb59c5f579d578b
DiptikaD <diptikadevi@gmail.com>
2025-02-25 09:16:39 -0500

    added methods from mvn test

--- maven/src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ maven/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,6 +1,8 @@
 package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,5 +11,17 @@ public class StepDefinitions {
     public void I_have_cukes_in_my_belly(int cukes) {
         Belly belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @When("I wait {int} hour")
+    public void i_wait_hour(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 }
