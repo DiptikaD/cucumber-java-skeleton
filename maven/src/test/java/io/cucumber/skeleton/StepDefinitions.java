f078c24df0b4e6ceffd3b7c01318ebe49febf108
DiptikaD <diptikadevi@gmail.com>
2025-02-25 09:18:16 -0500

    adjusted for belly class

--- maven/src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ maven/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -7,9 +7,12 @@ import io.cucumber.java.en.When;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StepDefinitions {
+
+    private Belly  belly;
+
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
-        Belly belly = new Belly();
+        belly = new Belly();
         belly.eat(cukes);
     }
 
